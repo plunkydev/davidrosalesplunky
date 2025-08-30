1c8c2780ce84dfec7c918e911f76ebcd84aad0f4
plunkydev <plunkydesign@gmail.com>
2025-08-29 21:16:49 -0500

    feat(hero): agrega lógica de scroll y resize para animar imgHero y ajustar altura de sección

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import './style.css'
-
+import { frames } from './data/api-images'
+import { igualarAltura } from './utils/heroIgualarAltura'
+import { updateImgHego } from './utils/updateImageHero'
 // Importes “fantasma” para que Webpack EMITA los 3 assets críticos
 // (el HTML los referencia con nombre fijo; aquí solo forzamos su inclusión en el build)
 import './assets/icons/favicon.ico'
 import './assets/apple-touch-icon.jpg'
 import './assets/preview-1200x630.png'
+//Generales--------------------------------------------------------------------------------------------------------------
+const imgHero = document.getElementById('imgHero');
+const sectionHeight = document.querySelector(".section-height");
+//Generales--------------------------------------------------------------------------------------------------------------
+//Area inicio--------------------------------------------------------------------------------------------------------------
+window.addEventListener("scroll", () => {
+    updateImgHego(imgHero, frames);
+}, 30);
+
+window.onload = () => {
+    if ('scrollRestoration' in history) {
+        history.scrollRestoration = 'manual';
+    }
+    window.scrollTo(0, 0);
+    imgHero.src = frames[0];
+    igualarAltura(imgHero, sectionHeight);
+};
+window.addEventListener('resize', () => igualarAltura(imgHero, sectionHeight));
+//Area inicio fin--------------------------------------------------------------------------------------------------------------
