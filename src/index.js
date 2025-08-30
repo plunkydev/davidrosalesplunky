5a252445cc57a4761bfc106c0fc7e361ad50376e
plunkydev <plunkydesign@gmail.com>
2025-08-30 11:19:34 -0500

    feat(js): animar barras de progreso en sectionSkills con IntersectionObserver y fondo dinámico

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import './assets/icons/favicon.ico'
 import './assets/apple-touch-icon.jpg'
 import './assets/preview-1200x630.png'
 import whiteLogo from './assets/logo-plunky-blanco.png'
+import bgImgSkills from './assets/fondo-skills.webp'
 //Generales--------------------------------------------------------------------------------------------------------------
 const imgHero = document.getElementById('imgHero');
 const sectionHeight = document.querySelector(".section-height");
@@ -17,9 +18,12 @@ const hamburguesa = document.getElementById('hamburguesa');
 const menu = document.getElementById('menu');
 const menuLinks = document.querySelectorAll('#menu a');
 const navLogo = document.getElementById('nav-logo');
+const bgSkills = document.querySelector('.bg-img');
+const bars = document.querySelectorAll('.progress-bar');
 let lastScroll = 0;
 window.onload = () => {
     imgHero.src = frames[0];
+    bgSkills.src = bgImgSkills;
     if ('scrollRestoration' in history) {
         history.scrollRestoration = 'manual';
     }
@@ -37,4 +41,25 @@ window.addEventListener("scroll", () => {
 }, 30);
 
 window.addEventListener('resize', () => igualarAltura(imgHero, sectionHeight));
+// Crea un observador que detecta cuando los elementos skills entran en la vista (viewport)
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        const bar = entry.target;
+        const percent = bar.getAttribute('data-percent');
+
+        if (entry.isIntersecting) {
+            // Cuando entra → animar al porcentaje
+            bar.style.width = percent + '%';
+        } else {
+            // Cuando sale → resetear a 0 para que pueda volver a animarse
+            bar.style.width = '0%';
+        }
+    });
+}, { threshold: 0.5 });
+
+bars.forEach(bar => {
+    bar.style.width = '0%';
+    bar.style.transition = 'width 2s ease';
+    observer.observe(bar);
+});
 //Area inicio fin--------------------------------------------------------------------------------------------------------------
