f09a0e79f79d4f19386c73600146cec0d4367c30
plunkydev <plunkydesign@gmail.com>
2025-09-01 16:28:49 -0500

    add: testimonio joxu y samanta

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -9,8 +9,10 @@ import 'swiper/css/pagination';
 
 // Importa tus imágenes para que Webpack las emita
 import t1 from '../assets/maryfel-alvarado.webp';
-import t2 from '../assets/testimonial-2.webp';
+import t2 from '../assets/sopy.webp';
 import t3 from '../assets/sony.webp';
+import t4 from '../assets/samanta.webp';
+import t5 from '../assets/Joxu.webp';
 
 // Datos por defecto (puedes pasarlos desde fuera si quieres)
 const DEFAULT_ITEMS = [
@@ -22,15 +24,27 @@ const DEFAULT_ITEMS = [
     },
     {
         img: t2,
-        nombre: 'Luis García',
-        cargo: 'CTO, BetaLabs',
-        texto: 'Código limpio, comunicación clara y foco en negocio. Repetiremos.'
+        nombre: 'Marcelo andree González Díaz',
+        cargo: 'Maestro Carpintero en Diseño de Cocinas y Clósets',
+        texto: 'David tiene un gran talento para transformar ideas en espacios únicos. Su visión, detalle y creatividad hacen que cada diseño de interiores cobre vida.'
     },
     {
         img: t3,
         nombre: 'Sony Valles',
         cargo: 'Diseñadora Gráfica Digital e Impresa',
         texto: 'Destaca por su madurez, compromiso y creatividad. Siempre atento, paciente y audaz, genera ideas brillantes y proyectos únicos con excelencia.'
+    },
+    {
+        img: t4,
+        nombre: 'Samanta Ruiz (Fabiana F.)',
+        cargo: 'SEO on page - Optimización SEO Blogs de empresas',
+        texto: 'Trabajar con David fue excelente: escuchó mis ideas, aportó propuestas innovadoras y entregó un sitio moderno, optimizado y fácil de usar. Su trato cercano y profesional hizo el proceso muy ameno. ¡Totalmente recomendable!'
+    },
+    {
+        img: t5,
+        nombre: 'Hilder Josue Nuñez',
+        cargo: 'Diseñador, Compositor y Productor Audiovisual',
+        texto: 'A David le das una idea y aparte de mejorarla la lleva a la realidad con procesos originales aparte despierta la creatividad en uno.'
     }
 ];
 
